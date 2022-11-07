fdb631b742df7c1b1ff1adc60d754e1bf74ab0e4
MaxEntroy <likang@tju.edu.cn>
2022-11-07 12:50:30 +0800

    beam-search: Add greedy search.

---
+++ main.cc
@@ -0,0 +1,59 @@
+// author: https://gist.github.com/mbant/67875e0464cd9d1402413532e3244261
+
+#include <array>
+#include <string>
+#include <utility>
+#include <vector>
+
+constexpr std::array<char, 26> kVocab{'a','b','c','d','e','f','g',
+                                      'h','i','j','k','l','m','n',
+                                      'o','p','q','r','s','t',
+                                      'u','v','w','x','y','z'};
+
+std::vector<std::vector<double>> log_p_language_model;
+double lm(char cur, char next) {
+  return log_p_language_model[cur - 'a'][next - 'a'];
+}
+
+// we want to decode a string of length 'length',
+// starting for a given character
+// and computing the next character by interfacing with our very sophisticated Language Model
+// lm(current,next) that will return us the probability that the 'next' character follows the 'current'
+// finally output the best string and its log_probability
+
+// we can do it greedily (not optimal but easier to code)
+
+struct Sequence {
+  std::string str;
+  double score;
+  Sequence() : score(0.0) {}
+  Sequence(const std::string& s, double score_in) : str(s), score(score_in) {}
+
+  bool operator<(const Sequence& rhs) const { return score < rhs.score; }
+};
+
+Sequence greedy_decoding(char start, int length) {
+  Sequence seq;
+  auto& s = seq.str;
+  s.reserve(length);
+  s.push_back(start);
+
+  char chosen = '\0';
+  double max_score = 0.0;
+  for (int i = 1; i < length; ++i) {
+    for (const auto& ch : kVocab) {
+      double tmp_score = lm(s.back(), ch);
+      if (tmp_score > max_score) {
+        max_score = tmp_score;
+        chosen = ch;
+      }
+    }
+    s.push_back(chosen);
+    seq.score += max_score;
+  }
+  return seq;
+}
+
+int main() {
+  return 0;
+}
